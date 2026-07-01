92e6f9d7c155a309996a23e719dc2f00e7b0c487
Sanaa-Abbas <ssmohamedahmedaabbas@gmail.com>
2026-07-01 14:57:21 +0400

    Add Book data model

--- app/src/main/java/com/bookshare/app/data/model/Book.kt
+++ app/src/main/java/com/bookshare/app/data/model/Book.kt
@@ -5,10 +5,20 @@ import com.google.gson.annotations.SerializedName
 
 data class Book(
     val id: Int,
+    val owner: String,
     val title: String,
     val author: String,
     val description: String,
+    val image: String?,
     val available: Boolean,
-    @SerializedName("owner_username")
-    val ownerName: String
+    val language: String,
+    val category: String,
+    val condition: String,
+
+
+    @SerializedName("created_at")
+    val createdAt: String,
+
+    @SerializedName("updated_at")
+    val updatedAt: String
 )
